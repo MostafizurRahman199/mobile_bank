dcd3963cc53d976fa1e83452fb10c2431a702ad8
MostafizurRahman199 <fardilshifat199@gmail.com>
2024-09-18 23:09:47 +0600

    login page design done

---
+++ js/login.js
@@ -0,0 +1,8 @@
+
+const loginBtn = document.getElementById("loginBtn");
+
+loginBtn.addEventListener("click", function () {
+    const pin = document.getElementById("pin").value;
+    const phoneNumber = document.getElementById("phoneNumber").value;
+  console.log(pin, phoneNumber);
+});
